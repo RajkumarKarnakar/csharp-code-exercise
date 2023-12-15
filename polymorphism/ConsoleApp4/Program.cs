29826a373f8624cb3d8924bad5e20302543c38cd
RajkumarKarnakar <147190172+RajkumarKarnakar@users.noreply.github.com>
2023-12-15 13:37:20 +0530

    oop concept in c#

---
+++ polymorphism/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Shape[] objshape= new Shape[3];
+            objshape[0] = new Shape();
+            objshape[1] = new Circle();
+            objshape[2] = new Rectangle();
+            foreach (Shape shape in objshape)
+            {
+                shape.draw();
+            }
+        }
+    }
+    public class Shape
+    {
+        public virtual void draw()=> Console.WriteLine("in shape draw");
+    }
+    public class Circle : Shape
+    {
+        public override void draw()=> Console.WriteLine("in draw of circle");
+    }
+    public class Rectangle : Shape
+    { 
+    public override void draw() => Console.WriteLine("in rectangle draw");
+    }
+}
