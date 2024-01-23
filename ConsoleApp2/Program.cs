fb6e3a4b99b60d8678ac9f6c26b9be273958fd1b
RajkumarKarnakar <147190172+RajkumarKarnakar@users.noreply.github.com>
2024-01-23 23:11:28 +0530

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp2
+{
+    class PublisherClass//Publisher Class
+    {
+        // Declare Delegate
+        public delegate void MyDelegate(string msg);
+        // Declaring Event
+        public event MyDelegate myevent;
+
+
+        public void displayMessageO()
+        {
+            myevent.Invoke("Welcome to India");// Raise event
+        }
+
+        class Program //Subscriber Class
+        {
+            //Event handler
+            //Delegate call this method when event raised
+
+            static void message(string str)
+            {
+                Console.WriteLine(str);
+            }
+            static void Main(string[] args)
+            {
+                PublisherClass obj = new PublisherClass();
+                // Event gets binded with delegate
+                obj.myevent += new PublisherClass.MyDelegate(message);
+                obj.displayMessageO();
+            }
+        }
+
+    }
+}
+
